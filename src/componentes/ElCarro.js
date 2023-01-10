36c502d4797a752b9cca56f41b6ab03111682da0
DanikMonday <daacostaf@unal.edu.co>
2023-01-09 21:22:55 -0500

    whatsapp v78

--- src/componentes/ElCarro.js
+++ src/componentes/ElCarro.js
@@ -46,64 +46,82 @@ export const ElCarro = () => {
   return (
     <div>
         <Bienvenida titulo='TU CARRITO'/>
+        <div className='gran-div'>
+            
 
-        <div className='carritos'>
-            <div className='carrito'>
-                {
-                    carrito.length === 0 ? <h2>Tu Carrito Está Vacío</h2>: <>
+            <div className='carritos'>
+                
                     {
-                    carrito.map((producto)=>(
-                        <>
-                        
-                        <div className="sec-descr-gen-carro">
+                        carrito.length === 0 ? <h2>Tu Carrito Está Vacío</h2>: <>
+                        {
+                        carrito.map((producto)=>(
+                            
+                            <div className="sec-descr-gen-carro">
 
-                            <div className="sec-descr-carro">
-                                
-                                    <div className="cont-prod-carro" key={producto.id}>
-                                        <div className='cont-izq-carro'>
-                                            
-                                            <div className="cont-img-prod-carro"><img className="img-prod" src={producto.image} alt="img carro" /></div> 
-                                            <div><p className="cop-prod-car">${producto.price}COP</p></div>
-                                            <div className='btn-seguir'><Link  to="/Productos" className='btn-seguir'>Seguir Comprando</Link></div>
-                                        </div>
-                                        <div>
-                                            <h2 className="nom-prod">{producto.title}</h2>
-                                            <p className="p-descr-carro">{producto.description}</p> 
-                                            <div className="cont-conta">
+                                <div className="sec-descr-carro">
+                                    
+                                        <div className="cont-prod-carro" key={producto.id}>
+                                            <div className='cont-izq-carro'>
                                                 
-                                                <button className="btn-sus" onClick={() => restar(producto.id)}>-</button>
-                                                <p className='cantidad'>{producto.cantidad}</p>
-                                                <button className="btn-adi" onClick={() => sumar(producto.id)}>+</button>
-                                                <div className="btn-basu"  onClick={()=>removeProducto(producto.id)}><BsTrashFill/></div>
+                                                <div className="cont-img-prod-carro"><img className="img-prod" src={producto.image} alt="img carro" /></div> 
+                                                <div><p className="cop-prod-car">${producto.price}COP</p></div>
+                                                <div className='btn-seguir'><Link  to="/Productos" className='btn-seguir'>Seguir Comprando</Link></div>
+                                            </div>
+                                            <div className='detalles'>
+                                                <h2 className="nom-prod">{producto.title}</h2>
+                                                <p className="p-descr-carro">{producto.description}</p> 
+                                                <div className="cont-conta">
+                                                    <div>Talla</div>
+                                                    <button className="btn-sus" onClick={() => restar(producto.id)}>-</button>
+                                                    <p className='cantidad'>{producto.cantidad}</p>
+                                                    <button className="btn-adi" onClick={() => sumar(producto.id)}>+</button>
+                                                    <div className="btn-basu"  onClick={()=>removeProducto(producto.id)}><BsTrashFill/></div>
+                                                </div>
+                                                
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className='res-carro'>
-                                        <h3>RESUMEN</h3>
-                                        <div className='res-carro-int'>
-                                        <p className='cantidad'>{producto.cantidad}</p>
-                                        <h2 className="nom-prod">{producto.title}</h2>
-                                        <p> {(producto.price*(producto.cantidad))}.000</p>
-                                        <div><Link to="/Resumen" className='btn-volver'>CONFIRMAR</Link></div>
-                                    </div>
-                                    </div>
-                            </div> 
-                     
-                               
-                        </div>
-                       
-                        </>
+                                    
+                                </div> 
+                        
+                                
+                            </div>
+                        
+                            
 
-                    ))
-                }  
+                        ))
+                    }  
+            
                 
-                <h3 className='total'>Total: ${total}.000 COP</h3>
-                <p className='entrega'>El tiempo de entrega depende de la cantidad de piezas solicitadas y las características específicas de cada producto, variando entre 15 y 30 días</p>
-                </>
-}
-            </div>
+                    </>}      
+                </div>   
 
-        </div>
+                <div className='carrito-der'>
+                        <h3>RESUMEN</h3>
+                        <div className='res-carro'>
+                        {
+                            carrito.map((producto)=>(
+                                <>
+                                                <div className='res-carro-int '>
+                                                    <p className='cantidad'>{producto.cantidad}</p>
+                                                    <h2 className="nom-prod">{producto.title}</h2>
+                                                    <p> {(producto.price*(producto.cantidad))}.000</p>  
+                                                </div>
+                                    
+                                
+                            
+                            
+                                </>
+
+                            ))
+                        }  
+                        <h3 className='total'>Total: ${total}.000 COP</h3>
+                        <div><Link to="/Resumen" className='btn-volver'>CONFIRMAR</Link></div>
+                        </div>
+                </div>
+
+        </div> 
+            <h3 className='total'>Total: ${total}.000 COP</h3>
+            <p className='entrega'>El tiempo de entrega depende de la cantidad de piezas solicitadas y las características específicas de cada producto, variando entre 15 y 30 días</p>
     </div>
   )
 }
--- src/componentes/Resumen.js
+++ src/componentes/Resumen.js
@@ -20,35 +20,41 @@ export const Resumen = () => {
             <div className='carrito'>
                 {
                     carrito.length === 0 ? <h2>Tu Carrito Está Vacío</h2>: <>
+                    <div className="sec-descr-gen-carro-res">
+                    <div className="sec-descr-res">
                     {
                     carrito.map((producto)=>(
                         <>
-                        
-                        <div className="sec-descr-gen-carro">
-
-                            <div className="sec-descr">
-                                
                                     <div className="cont-prod-res" key={producto.id}>
                                             <div className='res-items'>
-                                                <div><p className='cantidad'>#{producto.cantidad}  </p></div>
+                                                <div><p className='cantidad'>{producto.cantidad} Und. </p></div>
                                                 <div><h2 className="nom-prod">.  {producto.title}</h2></div>
                                                 <div><p>.  COP {(producto.price*(producto.cantidad))}.000</p></div>
                                             </div>
+                                    </div>
+                        
+                        </>
 
-                                            <h3 className='total'>Total: ${total}.000 COP</h3>
-                                             <ReactWhatsapp number='+57-316-406-6946' message={`"hi" ${carrito.map(item =>producto.cantidad + producto.title)}`}><div><Link to="/Resumen" className='btn-wa'>ENVIAR PEDIDO VÍA WhatsApp...<BsWhatsapp></BsWhatsapp></Link></div>
+                    ))
+                }   
+                <h3 className='total'>Total: ${total}.000 COP</h3>
+
+                {
+                    carrito.map((producto)=>(
+                        <>
+                                    <div className="cont-prod-res" key={producto.id}>
+                                             <ReactWhatsapp number='+57-316-406-6946' message={`"Hola, revisé tu página web y me interesan estos productos:" ${carrito.map(item =>producto.cantidad + producto.title)}`}><div><Link to="/Resumen" className='btn-wa'>ENVIAR PEDIDO VÍA WhatsApp...<BsWhatsapp></BsWhatsapp></Link></div>
                                            </ReactWhatsapp>
                                     </div>
-                                
-                            </div> 
-                               
-                        </div>
                         
-                        <p className='entrega'>El tiempo de entrega depende de la cantidad de piezas solicitadas y las características específicas de cada producto, variando entre 15 y 30 días</p>
                         </>
 
                     ))
                 }
+                    </div> 
+                 </div>
+               
+                <p className='entrega'>El tiempo de entrega depende de la cantidad de piezas solicitadas y las características específicas de cada producto, variando entre 15 y 30 días</p>
                 </>
 }
             </div>
